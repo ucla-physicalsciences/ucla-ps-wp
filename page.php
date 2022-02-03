65d0b4a46bf6fd768c0e9f57a9e2b22551680450
Scott Gruber <scott@scottgruber.me>
2022-02-03 11:05:34 -0800

    update changes from ps theme to parent theme

--- page.php
+++ page.php
@@ -6,18 +6,17 @@
   ?>
 
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-    <div class="breadcrumb"><?php get_breadcrumb(); ?> / <?php the_title(); ?></div>
+    <header>
+    <p class="breadcrumb"><?php get_breadcrumb(); ?> / <?php the_title(); ?></p>
     <h1><?php the_title(); ?></h1>
-
+    </header>
     <div class="entry-content">
 
       <?php get_template_part( 'template-parts/acf-blocks-ucla-wc' ); ?>
       <?php the_content(); ?>
 
       <?php if (is_active_sidebar('right-widget-area')) : ?>
-      <div class="col span_12_of_12"></div>
-      <div class="col span_12_of_12">
+      <div class="sidebar-widget">
         <?php dynamic_sidebar('right-widget-area'); ?>
       </div>
       <?php endif; ?>
