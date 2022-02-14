e4c2273586cdce211232395ce8e473c6f306e662
Scott Gruber <scott@scottgruber.me>
2022-02-14 15:19:01 -0800

    Format

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -7,7 +7,6 @@
  * @package ucla-ps-wp
  * 
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -16,17 +15,9 @@
 		<?php the_post_thumbnail(); ?>
 		<?php the_title( '<h1>', '</h1>' ); ?>
 	</header>
-
+  <?php get_template_part( 'template-parts/acf-blocks' );	?>
 	<div class="entry-content">
-		<?php
-		the_content();
-		get_template_part( 'template-parts/acf-blocks-ucla-wc' );	
-		?>
-	</div><!-- .entry-content -->
+	<?php the_content(); ?>
+	</div>
 
-	
-		
-
-
-
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
