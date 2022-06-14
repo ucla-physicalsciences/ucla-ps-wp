1a7544e12059b5a7f5a2a0499bf44526340fa59c
Scott Gruber <scott@scottgruber.me>
2022-06-14 15:47:47 -0700

    Add if statement to wrap in figure element

--- template-parts/header/featured-image.php
+++ template-parts/header/featured-image.php
@@ -4,12 +4,17 @@
  *
 **/
 ?>
-
+<?php
+if ( has_post_thumbnail() ): ?>
 <figure class="c-hero-image">
+<?php endif; ?>	
 <?php the_post_thumbnail( 'large', ['class' => 'img-responsive'] ); ?>
 <?php $caption = get_the_post_thumbnail_caption() ?>
 <?php if ( $caption ): ?>
 	<figcaption><?php echo esc_html( $caption ); ?></figcaption>
 <?php endif; ?>
+<?php
+if ( has_post_thumbnail() ): ?>
 </figure>
+<?php endif; ?>
 
