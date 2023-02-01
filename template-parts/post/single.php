d50fe8dc90cef4d9cb55a774b380f3b8997ca4d7
Scott Gruber <scott@scottgruber.me>
2023-02-01 14:56:12 -0800

    Add single post layout

--- template-parts/post/single.php
+++ template-parts/post/single.php
@@ -4,11 +4,18 @@
 */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article data-layout="c-single-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header">
-		<?php the_post_thumbnail(); ?>
-		<?php the_title( '<h1>', '</h1>' ); ?>
+		 <figure>
+				<?php the_post_thumbnail( 'medium', ['class' => 'u-photo c-post-img'] ); ?>
+				<?php if (the_post_thumbnail_caption()): ?>
+    			<figcaption><?php the_post_thumbnail_caption(); ?></figcaption>
+    		<?php endif; ?>
+		 </figure>
+		<div>
+			<?php the_title( '<h1>', '</h1>' ); ?>
+		</div>
 	</header>
   <div class="entry-content">
 	<?php the_content(); ?>
